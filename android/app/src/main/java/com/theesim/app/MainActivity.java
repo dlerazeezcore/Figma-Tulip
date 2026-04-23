96b2960689690eb8fdc579d9bbf57a687ecced0f
dlerazeezcore <dler@corevia-consultants.com>
2026-04-23 03:34:33 -0500

    feat: Configure Android app for Capacitor and Firebase
    Initialize Firebase within the Android MainActivity and update the app/build.gradle to include necessary configurations for Capacitor and Firebase integration.

---
+++ android/app/src/main/java/com/theesim/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.theesim.app;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
+    }
+}
